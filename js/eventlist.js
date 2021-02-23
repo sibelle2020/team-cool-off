b48c11d5a3ea1c8f26613a9217646285e25ce3b5
Sandra Sibelle <sibellemusic@gmail.com>
2021-02-23 12:19:39 +0100

    added javascript popular events

--- js/eventlist.js
+++ js/eventlist.js
@@ -1,7 +1,8 @@
 //KEY: 6033bd605ad3610fb5bb64f6
 //ENDPOINT: https://s21kea-d06b.restdb.io/rest/events
 
-const url = "https://s21kea-d06b.restdb.io/rest/events";
+//const url = "https://s21kea-d06b.restdb.io/rest/events";
+const url = "https://s21kea-d06b.restdb.io/rest/events?sort=popularity&max=3";
 
 const options = {
   headers: {
@@ -19,5 +20,21 @@ fetch(url, options)
   });
 
 function testList(data) {
-  data.forEach(showEvent);
+  data.forEach(showPopular);
+}
+
+function showPopular(popular) {
+  console.log(popular);
+  //grab the template
+  const template = document.querySelector("#popular-template").content;
+  //clone it
+  const copy = template.cloneNode(true);
+
+  //change content
+  copy.querySelector("h3").textContent = popular.artistname;
+
+  //grab parent
+  const parent = document.querySelector(".events-wrapper");
+  //append
+  parent.appendChild(copy);
 }
