721057a70a3586214ff8fe5351c4926035e2d6dc
Sonia-Wa <soniawachowiak01@gmail.com>
2021-02-24 14:04:21 +0100

    main page changes

---
+++ main.js
@@ -0,0 +1,41 @@
+//KEY: 6033bd605ad3610fb5bb64f6
+//ENDPOINT: https://s21kea-d06b.restdb.io/rest/events
+
+const urlParams = new URLSearchParams(window.location.search);
+let url =
+  "https://s21kea-d06b.restdb.io/rest/events?sort=popularity&max=3";
+  
+const options = {
+    headers: {
+      "x-apikey": "6033bd605ad3610fb5bb64f6",
+    },
+  };
+
+fetch(url, options)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    popularList(data);
+});
+
+function popularList(data) {
+    data.forEach(showPopular);
+}
+
+function showPopular(popular) {
+    console.log(popular);
+    //grab the template
+    const template = document.querySelector("#popular-template").content;
+    //clone it
+    const copy = template.cloneNode(true);
+  
+    //change content
+    copy.querySelector("h3").textContent = popular.artist;
+    copy.querySelector("h4").textContent = popular.eventname;
+
+    //grab parent
+    const parent = document.querySelector(".events-wrapper");
+    //append child
+    parent.appendChild(copy);
+}
